b5df7f92b7bd0a54a1498673099993406fdd8e6c
Edmund Korley <korley.em@gmail.com>
2016-10-13 16:50:54 -0700

    Basic static version with button assets

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        recordingLabel.text = "Now recording!"
+        stopRecordingButton.isEnabled = true
+        recordingButton.isEnabled = false
+    }
 
+    @IBAction func stopRecording(_ sender: AnyObject) {
+        recordingLabel.text = "Finished recording 😀"
+        stopRecordingButton.isEnabled = false
+        recordingButton.isEnabled = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordingButton.isEnabled = false
+    }
 }
 
