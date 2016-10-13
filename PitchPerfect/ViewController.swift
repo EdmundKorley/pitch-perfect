d80674a2fca341180a9f8b78f18c39206c438a15
Edmund Korley <korley.em@gmail.com>
2016-10-13 13:16:58 -0700

    First commit 🎊

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Edmund Korley on 2016-10-13.
+//  Copyright © 2016 Edmund Korley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
